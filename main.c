0179b3e13f9d5439e4fa67f1cd3749760107f407
nori <nori623p@gmail.com>
2022-04-01 15:11:13 +0200

    refact

--- main.c
+++ main.c
@@ -10,6 +10,13 @@
 
 #define HASH_BASE_SIZE 1000000
 
+typedef struct hotrace
+{
+	t_hash	*dict;
+	t_clist	*kvlst;
+	t_clist	*keylst;
+}	t_hotrace;
+
 void	hr_puterr(char *place)
 {
 	ft_putstr_fd(place, STDERR_FILENO);
@@ -17,75 +24,97 @@ void	hr_puterr(char *place)
 	ft_putstr_fd(strerror(errno), STDERR_FILENO);
 }
 
-void	hr_handle_err( char *err_place, t_hash **dictp, t_clist **lstp)
+void	destroy_hotrace(t_hotrace *hr)
 {
-	ft_clst_clear(lstp, free);
-	hash_destroy(dictp);
-	hr_puterr(err_place);
+	hash_destroy(&hr->dict);
+	ft_clst_clear(&hr->kvlst, free);
+	ft_clst_clear(&hr->keylst, free);
 }
 
-t_hash	*parse_lst_to_dict(t_clist *kvlst)
+bool	init_hotrace(t_hotrace *hr)
 {
-	t_hash	*dict;
-	char	*key;
-	char	*value;
-
-	dict = hash_new(HASH_BASE_SIZE);
-	if (!dict)
+	hr->dict = hash_new(HASH_BASE_SIZE);
+	hr->kvlst = ft_clstnew(NULL);
+	hr->keylst = ft_clstnew(NULL);
+	if (!hr->dict || !hr->kvlst || !hr->keylst)
 	{
-		hr_handle_err("hash new", NULL, &kvlst);
-		return (NULL);
+		hr_puterr("init");
+		return (false);
 	}
-	kvlst = ft_clstfirst(kvlst);
-	while (!ft_clst_isend(kvlst))
-	{
-		key = kvlst->data;
-		value = kvlst->next->data;
-		if (!hash_add(dict, key, value))
-		{
-			hr_handle_err("hash add", &dict, &kvlst);
-			return (NULL);
-		}
-		if (ft_clst_isend(kvlst->next))
-			break ;
-		kvlst = kvlst->next->next;
-	}
-	return (dict);
+	return (true);
 }
 
-t_clist	*read_lines_to_kvlst()
+bool	read_into_lst(t_clist *lst, bool (*is_empty)())
 {
-	t_clist	*kvlst;
-	char	*line;
 	int		ret;
+	char	*line;
 
-	kvlst = ft_clstnew(NULL);
-	if (!kvlst)
-	{
-		hr_puterr("lstnew");
-		return (NULL);
-	}
 	ret = 1;
 	while (ret)
 	{
 		ret = get_next_line(STDIN_FILENO, &line);
 		if (ret < 0)
 		{
-			hr_handle_err("gnl", NULL, &kvlst);
-			return (NULL);
+			hr_puterr("gnl");
+			return (false);
 		}
-		if (!line[0])
+		if (is_empty(line, ret))
 		{
 			free(line);
 			break ;
 		}
-		if (!ft_clstnew_add_back(kvlst, line))
+		if (!ft_clstnew_add_back(lst, line))
 		{
-			hr_handle_err("lstnew", NULL, &kvlst);
-			return (NULL);
+			free(line);
+			hr_puterr("lstnew");
+			return (false);
 		}
 	}
-	return (kvlst);
+	return (true);
+}
+
+bool	is_empty_for_kvlst(char *line, int ret)
+{
+	(void)ret;
+	return (line[0] == '\0');
+}
+
+bool	is_empty_for_keylst(char *line, int ret)
+{
+	return (ret == 0 && line[0] == '\0');
+}
+
+bool	read_into_kvlst(t_clist *kvlst)
+{
+	return (read_into_lst(kvlst, is_empty_for_kvlst));
+}
+
+bool	read_into_keylst(t_clist *keylst)
+{
+	return (read_into_lst(keylst, is_empty_for_keylst));
+}
+
+bool	parse_kvlst_to_hash(t_hotrace *hr)
+{
+	char	*key;
+	char	*value;
+	t_clist	*kvlst;
+
+	kvlst = ft_clstfirst(hr->kvlst);
+	while (!ft_clst_isend(kvlst))
+	{
+		key = kvlst->data;
+		value = kvlst->next->data;
+		if (!hash_add(hr->dict, key, value))
+		{
+			hr_puterr("hash add");
+			return (false);
+		}
+		if (ft_clst_isend(kvlst->next))
+			break ;
+		kvlst = kvlst->next->next;
+	}
+	return (true);
 }
 
 void	print_value(char *key, char *value)
@@ -99,50 +128,47 @@ void	print_value(char *key, char *value)
 	}
 }
 
-int print_searched_values(t_hash* dict)
+void	print_values(t_hotrace *hr)
 {
 	char	*key;
 	char	*value;
-	int		ret;
+	t_hash	*dict;
+	t_clist	*keylst;
 
-	ret = 1;
-	while (ret)
+	dict = hr->dict;
+	keylst = ft_clstfirst(hr->keylst);
+	while (!ft_clst_isend(keylst))
 	{
-		ret = get_next_line(STDIN_FILENO, &key);
-		if (ret < 0)
-		{
-			hr_puterr("gnl");
-			return (1);
-		}
+		key = keylst->data;
 		value = hash_get(dict, key);
 		print_value(key, value);
-		free(key);
+		keylst = keylst->next;
 	}
-	return (0);
 }
 
-t_hash	*create_dict()
+bool	read_into_dict(t_hotrace *hr)
 {
-	t_hash	*dict;
-	t_clist	*kvlst;
+	bool	is_ok;
 
-	kvlst = read_lines_to_kvlst();
-	if (!kvlst)
-		return (NULL);
-	dict = parse_lst_to_dict(kvlst);
-	ft_clst_clear(&kvlst, free);
-	return (dict);
+	is_ok = \
+		read_into_kvlst(hr->kvlst) && \
+		parse_kvlst_to_hash(hr);
+	ft_clst_clear(&hr->kvlst, free);
+	return (is_ok);
 }
 
 int main()
 {
-	t_hash	*dict;
-	int		res;
+	t_hotrace	hr;
 
-	dict = create_dict();
-	if (!dict)
-		return (1);
-	res = print_searched_values(dict);
-	hash_destroy(&dict);
-	return (res);
+	if (!init_hotrace(&hr) || \
+		!read_into_dict(&hr) || \
+		!read_into_keylst(hr.keylst))
+	{
+		destroy_hotrace(&hr);
+		return (EXIT_FAILURE);
+	}
+	print_values(&hr);
+	destroy_hotrace(&hr);
+	return (EXIT_SUCCESS);
 }
---
+++ main_prev.c
@@ -0,0 +1,148 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include "ft_io.h"
+#include "ft_string.h"
+#include "get_next_line.h"
+#include "hash.h"
+
+#define HASH_BASE_SIZE 1000000
+
+void	hr_puterr(char *place)
+{
+	ft_putstr_fd(place, STDERR_FILENO);
+	ft_putstr_fd(" :", STDERR_FILENO);
+	ft_putstr_fd(strerror(errno), STDERR_FILENO);
+}
+
+void	hr_handle_err( char *err_place, t_hash **dictp, t_clist **lstp)
+{
+	ft_clst_clear(lstp, free);
+	hash_destroy(dictp);
+	hr_puterr(err_place);
+}
+
+t_hash	*parse_lst_to_dict(t_clist *kvlst)
+{
+	t_hash	*dict;
+	char	*key;
+	char	*value;
+
+	dict = hash_new(HASH_BASE_SIZE);
+	if (!dict)
+	{
+		hr_handle_err("hash new", NULL, &kvlst);
+		return (NULL);
+	}
+	kvlst = ft_clstfirst(kvlst);
+	while (!ft_clst_isend(kvlst))
+	{
+		key = kvlst->data;
+		value = kvlst->next->data;
+		if (!hash_add(dict, key, value))
+		{
+			hr_handle_err("hash add", &dict, &kvlst);
+			return (NULL);
+		}
+		if (ft_clst_isend(kvlst->next))
+			break ;
+		kvlst = kvlst->next->next;
+	}
+	return (dict);
+}
+
+t_clist	*read_lines_to_kvlst()
+{
+	t_clist	*kvlst;
+	char	*line;
+	int		ret;
+
+	kvlst = ft_clstnew(NULL);
+	if (!kvlst)
+	{
+		hr_puterr("lstnew");
+		return (NULL);
+	}
+	ret = 1;
+	while (ret)
+	{
+		ret = get_next_line(STDIN_FILENO, &line);
+		if (ret < 0)
+		{
+			hr_handle_err("gnl", NULL, &kvlst);
+			return (NULL);
+		}
+		if (!line[0])
+		{
+			free(line);
+			break ;
+		}
+		if (!ft_clstnew_add_back(kvlst, line))
+		{
+			hr_handle_err("lstnew", NULL, &kvlst);
+			return (NULL);
+		}
+	}
+	return (kvlst);
+}
+
+void	print_value(char *key, char *value)
+{
+	if (value)
+		ft_putendl_fd(value, STDOUT_FILENO);
+	else
+	{
+		ft_putstr_fd(key, STDOUT_FILENO);
+		ft_putendl_fd(": Not found.", STDOUT_FILENO);
+	}
+}
+
+int print_searched_values(t_hash* dict)
+{
+	char	*key;
+	char	*value;
+	int		ret;
+
+	ret = 1;
+	while (ret)
+	{
+		ret = get_next_line(STDIN_FILENO, &key);
+		if (ret < 0)
+		{
+			hr_puterr("gnl");
+			return (1);
+		}
+		value = hash_get(dict, key);
+		print_value(key, value);
+		free(key);
+	}
+	return (0);
+}
+
+t_hash	*create_dict()
+{
+	t_hash	*dict;
+	t_clist	*kvlst;
+
+	kvlst = read_lines_to_kvlst();
+	if (!kvlst)
+		return (NULL);
+	dict = parse_lst_to_dict(kvlst);
+	ft_clst_clear(&kvlst, free);
+	return (dict);
+}
+
+int main()
+{
+	t_hash	*dict;
+	int		res;
+
+	dict = create_dict();
+	if (!dict)
+		return (1);
+	res = print_searched_values(dict);
+	hash_destroy(&dict);
+	return (res);
+}
